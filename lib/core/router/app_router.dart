d4eca21ae1fef6cd57440c150976c69f3dfc8cdb
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2025-04-22 20:39:21 +0700

    feat : add routing app

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,13 +1,11 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:tawassuth/core/router/app_router.gr.dart';
+import 'app_router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    AutoRoute(page: HomeRoute.page, initial: true),
-    AutoRoute(page: ScholarsRoute.page),
-    AutoRoute(page: ScholarsDetailRoute.page),
+    AutoRoute(page: LoginRoutes.page, initial: true),
     // add new routes here
   ];
 }
